7f61593592bf341235031cc9ac6efa38756c72b6
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-16 11:09:17 +0200

    profile component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { ExpirienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExpirienceBar />
+      <section>
+        <div>
+          <Profile />
+        </div>
+        <div></div>
+      </section>
     </div>
   )
 }
