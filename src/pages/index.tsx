a3bd7eabe06f1a2448aff88de747fdd340d6c754
Luccas Mesquita <luccas.m.mesquita@hotmail.com>
2021-04-16 12:06:39 +0200

    Countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,23 @@
 import { CompletedChallengens } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExpirienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
+import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Inicio | Move.it</title>
+      </Head>
       <ExpirienceBar />
       <section>
         <div>
           <Profile />
           <CompletedChallengens />
+          <Countdown />
         </div>
         <div></div>
       </section>
